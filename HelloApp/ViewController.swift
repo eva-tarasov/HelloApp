a80da28dd7e5a7ead16b643167980c872c3b8363
Egeniy Tarasov <tarasov@ucontent.ru>
2020-05-20 23:22:32 +0300

    Initial Commit

---
+++ HelloApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HelloApp
+//
+//  Created by Евгений Тарасов on 20.05.2020.
+//  Copyright © 2020 Евгений Тарасов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // Some changes
+
+}
+
